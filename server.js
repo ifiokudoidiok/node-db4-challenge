8790b9479440d68c1c77dd41b7e2f1fd7ac3f1ba
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-07 19:55:05 +0100

    router imported

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-// const recipeBookRouter = require('./config/router');
+const recipeBookRouter = require('./config/router');
 
 const server = express();
 
 server.use(express.json());
-// server.use('/api', recipeBookRouter);
+server.use('/api', recipeBookRouter);
 
 module.exports = server;
