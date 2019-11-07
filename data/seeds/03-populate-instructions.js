0b4ff7776ee539be6aecf3fb191f652f6df39f7d
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-07 19:56:00 +0100

    seed file bumped

--- data/seeds/03-populate-instructions.js
+++ data/seeds/03-populate-instructions.js
@@ -5,10 +5,10 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('instructions').insert([
-        { instruction_text: 'Boil for 10minutes'},
-        { instruction_text: 'Break as needed or put whole'},
-        { instruction_text: 'put in pan'},
-        { instruction_text: 'put in hot water'}
+        { instruction_text: 'Boil for 10minutes', ingredient_id:7},
+        { instruction_text: 'Break as needed or put whole', ingredient_id:3},
+        { instruction_text: 'put in pan', ingredient_id:8},
+        { instruction_text: 'put in hot oil', ingredient_id:5}
       ]);
     });
 };
